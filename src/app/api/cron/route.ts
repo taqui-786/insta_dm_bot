5fdaa295e792558d9f725a5b25886347953d1b66
taqui-786 <mdtaqui.jhar@gmail.com>
2025-02-25 11:50:07 +0530

    some changes done

--- src/app/api/cron/route.ts
+++
@@ -1,7 +0,0 @@
-import { handleAutoReply } from '@/lib/Instagram';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    await handleAutoReply()
-  return NextResponse.json({ ok: true });
-}
